c0e87d8aa856057caeaf53ec7c007c2d8045f4db
Aviral <aviraldubey2000@gmail.com>
2022-07-31 22:52:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom';import './App.css';
+import Electronics from './component/Electronics';
+import Fashion from './component/Fashion';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/electronics" element={<Electronics/>} />
+      <Route path="/fashion" element={<Fashion />} />
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/component/Electronics.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+
+function Electronics() {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+        <div class="card mb-3">
+  <img src={require("../essentials/electronics.jpg")} style={{height:"300px"}} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+        </div>
+    </div>
+  )
+}
+
+export default Electronics
